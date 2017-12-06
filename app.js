16ab269cec2a6bd38a10cc745051d67615536271
daliyajohn <daliyajohn75@gmail.com>
2017-12-06 09:55:41 +0530

    Connect mongoose

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/ngrx-db');
+
 app.get('/', (req, res) => {
     res.send('hey');
 });

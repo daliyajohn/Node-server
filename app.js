196154d2ab933b2506f21f4aa7054e9c318ab186
daliyajohn <daliyajohn75@gmail.com>
2017-12-08 09:15:03 +0530

    Add bodyparse

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/ngrx-db');
 
+
 app.get('/', (req, res) => {
     res.send('hey');
 });
+
+
 
 app.listen(3000, () => {
     console.log('running on 3000');
